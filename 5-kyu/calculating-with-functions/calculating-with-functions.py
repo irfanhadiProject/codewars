2757851de5b912bbdf9a409eb9f557e941fc3818
Irfan Hadi <145238150+irfanhadiProject@users.noreply.github.com>
2025-07-11 13:50:20 +0700

    Add solution - CodeHub

---
+++ 5-kyu/calculating-with-functions/calculating-with-functions.py
@@ -0,0 +1,15 @@
+def zero(f=None): return 0 if not f else f(0) #your code here
+def one(f=None): return 1 if not f else f(1)#your code here
+def two(f=None): return 2 if not f else f(2)#your code here
+def three(f=None): return 3 if not f else f(3)#your code here
+def four(f=None): return 4 if not f else f(4)#your code here
+def five(f=None): return 5 if not f else f(5)#your code here
+def six(f=None): return 6 if not f else f(6)#your code here
+def seven(f=None): return 7 if not f else f(7)#your code here
+def eight(f=None): return 8 if not f else f(8)#your code here
+def nine(f=None): return 9 if not f else f(9)#your code here
+​
+def plus(b): return lambda a: a + b  #your code here
+def minus(b): return lambda a: a - b #your code here
+def times(b): return lambda a: a * b #your code here
+def divided_by(b): return lambda a: a // b #your code here
